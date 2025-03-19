055190546cafd27adbcfa93f70c91b99093c1cfe
thomas-ravache <thomas.santos@bingaodobrasil.com.br>
2025-03-19 09:23:09 -0300

    feat: add JsonPropertyName attribute to B2BType in RequestBase

--- src/BM.B2BProvider.Application/Models/RequestBase.cs
+++ src/BM.B2BProvider.Application/Models/RequestBase.cs
@@ -1,8 +1,10 @@
+using System.Text.Json.Serialization;
 using BM.B2BProvider.Domain.Enums;
 
 namespace BM.B2BProvider.Application;
 
 public abstract class RequestBase
 {
+    [JsonPropertyName("b2bType")]
     public EB2BTypes B2BType { get; set; }
 }
